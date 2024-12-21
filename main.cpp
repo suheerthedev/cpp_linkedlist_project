6495a195a4b303992918dc531a682e15b9dec367
Suheer Khan <khansuheer1@gmail.com>
2024-12-21 18:45:44 +0500

    Made a function in linkedlist struct to add a new node

--- main.cpp
+++ main.cpp
@@ -11,6 +11,23 @@ struct LinkedList{
     Node* start;
     Node* last;
     int length = 0;
+
+    void addANewNode(int value){
+        Node* n = new Node;
+        n->data = value;
+        n->index = length;
+        n->next = nullptr;
+
+        if(length == 0){
+            start = n;
+            last = n;
+        }
+        else{
+            last->next = n;
+            last = n;
+        }
+        length++;
+    }
 };
 
 int main(){
